191ae31941322a1cd8b3858e4a024db58a9be9b0
KO527 <Kwajo.oseiakoto@gmail.com>
2015-12-24 01:48:43 -0500

    updated events config

---
+++ public/modules/core/config/events.client.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('events').config(['$stateProvider', function($stateProvider){
+		$stateProvider.state('event', {
+			url: '/events',
+			templateUrl: '/modules/events/views/events.client.view.html'
+		});
+	}
+]);
+
